9dcc335080e783a636e4ada5efaf4124e8caafb9
Ceren <cerenctn@hotmail.co.uk>
2021-07-24 23:21:30 +0100

    Change file path

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -7,7 +7,7 @@ const InputTodo = () => {
     e.preventDefault();
     try {
       const body = { description };
-      const response = await fetch("/todos", {
+      const response = await fetch("/todo", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body)
